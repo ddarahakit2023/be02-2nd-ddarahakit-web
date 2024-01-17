91711483201a10319d7923d23dee23f40ce4eb9e
ddarahakit <ddarahakit@gmail.com>
2024-01-17 12:28:55 +0900

    [Fix] 섹션과 강의의 이름에 unique 제거
    섹션과 강의 제목은 중복 가능하도록 변경

---
+++ src/main/java/com/ddarahakit/web/order/model/Orders.java
@@ -0,0 +1,41 @@
+package com.ddarahakit.web.order.model;
+
+import com.ddarahakit.web.user.model.User;
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Orders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(updatable = false, nullable = false)
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "User_id")
+    private User user;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = Timestamp.from(Instant.now());
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+}
---
+++ src/main/java/com/ddarahakit/web/order/model/OrdersCourse.java
@@ -0,0 +1,46 @@
+package com.ddarahakit.web.order.model;
+
+import com.ddarahakit.web.course.model.Course;
+import com.ddarahakit.web.user.model.User;
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrdersCourse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(updatable = false, nullable = false)
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "Orders_id")
+    private Orders orders;
+
+    @ManyToOne
+    @JoinColumn(name = "Course_id")
+    private Course course;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = Timestamp.from(Instant.now());
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+}
